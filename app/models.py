d58e531625f927955583f55372984f1b042bf866
Juan Carlos (jcgomez) <jcgomez@auvik.com>
2022-12-25 01:51:13 +0100

    Working locally; k8s pending

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
+from logpipe import Producer
 from rest_framework import serializers
 import uuid
 
@@ -10,14 +9,51 @@ class Person(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
+
+# Register admin
+from django.contrib import admin
+
+admin.site.register(Person)
+
+
+# TODO: Change to hyperlinked
 class PersonSerializer(serializers.ModelSerializer):
     MESSAGE_TYPE = 'person'
     VERSION = 1
     KEY_FIELD = 'uuid'
+    PersonProducer: Producer = None
 
     class Meta:
         model = Person
         fields = ['uuid', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        '''
+        Example of message in kafka...
+        {
+          "topic": "people",
+          "partition": 0,
+          "offset": 14,
+          "tstype": "create",
+          "ts": 1671929245526,
+          "broker": 0,
+          "key": "ddbf482b-62ab-4e80-83df-d60e43a1a19c",
+          "payload": "json:{\"type\":\"person\",\"version\":1,\"message\":{\"uuid\":\"ddbf482b-62ab-4e80-83df-d60e43a1a19c\",\"first_name\":\"Joe-s33\",\"last_name\":\"Schmoe\"}}"
+        }
+        '''
+        self.assertProducer()
+        instance = Person.objects.create(**validated_data)
+        self.PersonProducer.send(instance)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        # After testing a bit I get that:
+        # - instance is previous
+        # - validated_data is incoming data
+        self.assertProducer()
+        self.PersonProducer.send(validated_data)
+        self.PersonProducer.send()
+        return super().update(instance, validated_data)
 
     @classmethod
     def lookup_instance(cls, uuid, **kwargs):
@@ -25,3 +61,7 @@ class PersonSerializer(serializers.ModelSerializer):
             return Person.objects.get(uuid=uuid)
         except models.Person.DoesNotExist:
             pass
+
+    def assertProducer(self):
+        if self.PersonProducer is None:
+            self.PersonProducer = Producer('people', PersonSerializer)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+
+from app.models import Person, PersonSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
